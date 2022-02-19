fc660dd5b4af0bc82ff0d7b9e60b74b4799ec779
Rafael Rodrigues <rrodrigues@allin.com.br>
2022-02-18 23:07:17 -0300

    [FIX] NO SHIPS

--- index.py
+++ index.py
@@ -136,20 +136,30 @@ def main():
       print("Connected", now)
       if(clickBtn(images['sign'], timeout=1)):
         print("Sign", now)
-      if(clickBtn(images['play'], timeout=1)):
-        print("Clicked play", now)
-      if(clickBtn(images['ok'], timeout=1)):
-        print("Clicked ok", now)
-    # if(clickBtn(images['remove-spaceship'], timeout=1)):
-    #   print("Remove Spaceship", now)
-    # if(clickBtn(images['newest'], timeout=1)):
-    #   if(clickBtn(images['max-ammo'], timeout=1,threshold = 0.9)):
-    #     print("SET MAX AMMO", now)
+    if(clickBtn(images['play'], timeout=1)):
+      print("Clicked play", now)
+    if(clickBtn(images['ok'], timeout=1)):
+      print("Clicked ok", now)
+
+    if(clickBtn(images['close'], timeout=0.2, threshold=0.9)):
+      print("CLOSE")    
+
+    if(clickBtn(images['no-ammo'], timeout=0.2)):
+      print(pyautogui.position())
+      if(clickBtn(images['remove-spaceship'], timeout=1)):
+        print("Remove Spaceship", now)
+    if(clickBtn(images['newest'], timeout=1)):
+      if(clickBtn(images['max-ammo'], timeout=1,threshold = 0.9)):
+        print("SET MAX AMMO", now)
     if(clickBtn(images['send-to-fight'], timeout=0.5, threshold=0.9)):
       print("send-to-fight", now)
-    if(clickBtn(images['15-15'], timeout=0.2, threshold = 1)):
-      if(clickBtn(images['fight-boss'], timeout=0.2)):
-        print("send-to-boss", now)
+      if(clickBtn(images['15-15'], timeout=0.2, threshold = 1)):
+        if(clickBtn(images['fight-boss'], timeout=0.2)):
+          print("send-to-boss", now)
+    else:
+      if(clickBtn(images['15-15'], timeout=0.2, threshold = 1)):
+        if(clickBtn(images['fight-boss'], timeout=0.2)):
+          print("send-to-boss", now)
     if(clickBtn(images['victory-confirm'], timeout=0.5)):
       print("victory-confirm", now)
     if(clickBtn(images['surrender-boss'], timeout=0.5, threshold = 1)):
@@ -162,12 +172,8 @@ def main():
         print("SEND TO REPAIR", now)
     if(clickBtn(images['lose-confirm'], timeout=0.5, threshold = 0.9)):
       print("lose-confirm", now)
-    if not(clickBtn(images['send-to-fight'], timeout=0.2, threshold = 1)):
-      print("have-no-spaceship", now)
-      if(clickBtn(images['select-15'], timeout=0.2, threshold = 1)):
-        print("select-15", now)
-        if(clickBtn(images['fight-boss'], timeout=0.2)):
-          print("send-to-boss", now)       
+
+
       
 if __name__ == '__main__':
   main()
