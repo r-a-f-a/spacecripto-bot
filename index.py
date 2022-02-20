db900734747b4d739cad572f79e82e2b2ca9b1e1
Rafael Rodrigues <rrodrigues@allin.com.br>
2022-02-20 17:05:20 -0300

    [NEW]

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-    
+from fileinput import close
 from cv2 import cv2
 from os import listdir
 import numpy as np
@@ -17,13 +18,42 @@ stream = open("config.yaml", 'r')
 c = yaml.safe_load(stream)
 ct = c['threshold']
 
+
+print("""\
+                   `. ___
+                    __,' __`.                _..----....____
+        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+,'________________                          \`-._`-','
+ `._              ```````````------...___   '-.._'-:
+    ```--.._      ,.                     ````--...__\-.
+            `.--. `-`                       ____    |  |`
+              `. `.                       ,'`````.  ;  ;`
+                `._`.        __________   `.      \ '__/`
+                   `-:._____/______/___/____`.     \  `
+                               |       `._    `.    \ 
+                               `._________`-.   `.   `.___
+                                             SSt  `------'`
+""")
+
+
+print("""\
+##################################################
+#             WALLET FOR DONATE                  #
+#------------------------------------------------#
+#  0x3D2aEc88A2c73a23a25D4259A3B97740bBD8d53F    #
+#------------------------------------------------#
+#              PLEASE SUPPORT US                 #
+##################################################
+""")
+
 resolution = c['resolution']
-print(resolution)
+print("CONFIGURATION[RESOLUTION]:", resolution)
 
 pause = c['time_intervals']['interval_between_moviments']
 pyautogui.PAUSE = pause
 
-# [{'left': 0, 'top': 0, 'width': 3000, 'height': 2560}, {'left': 1080, 'top': 0, 'width': 1920, 'height': 1080}, {'left': 0, 'top': 0, 'width': 1080, 'height': 2560}]
+
 def moveTo(x,y,t):
     pyautogui.moveTo(x + diff_monitor,y,t)
 
@@ -35,7 +65,7 @@ def remove_suffix(input_string, suffix):
 def load_images(dir_path='./targets/'):
     global monitor_size
     pathSize =  dir_path + monitor_size + "/"
-    print("PATH: ", pathSize)
+    print("IMAGES FOLDER: ", pathSize)
     file_names = listdir(pathSize)
     targets = {}
     for file in file_names:
@@ -55,13 +85,11 @@ def printScreen():
           diff_monitor = monitor["left"]
         sct_img = np.array(sct.grab(monitor))
         monitor_size = str(monitor['width']) + "x" + str(monitor['height'])
-        # Grab the data
         return sct_img[:,:,:3]
 
 def positions(target, threshold=ct['default'],img = None):
     if img is None:
         img = printScreen()
-    # print("IMG", img)
     result = cv2.matchTemplate(img,target,cv2.TM_CCOEFF_NORMED)
     w = target.shape[1]
     h = target.shape[0]
@@ -94,26 +122,25 @@ def clickBtn(img, timeout=3, threshold = ct['default']):
         x,y,w,h = matches[0]
         pos_click_x = x+w/2
         pos_click_y = y+h/2
-        # print("POSITION: ", x,y,w,h)
-        # [793 790 335  41]
-        moveTo(pos_click_x,pos_click_y,1)
+    
+        moveTo(pos_click_x,pos_click_y,0.3)
         pyautogui.click()
-        moveTo(pos_click_x + 100,pos_click_y + 100,1)
+        moveTo(pos_click_x + 100,pos_click_y + 100,0.3)
         return True
 
     return False
+
+def checkImage(img, threshold=ct['default']):
+    matches = positions(img, threshold=threshold)
+    if(len(matches)==0):
+        return False
+    return True
 
 def scroll(images):
     
     scroll = positions(images['scroll-spaceship'], threshold = 1)
     x,y,w,h = scroll[0]
-    print(scroll, x,y,w,h)
     
-#     if (len(commoms) == 0):
-#         return
-#     x,y,w,h = commoms[len(commoms)-1]
-# #
-#     moveToWithRandomness(x,y,1)
     empty_scrolls_attempts = c['scroll']['attemps']
 
     while(empty_scrolls_attempts >0):
@@ -129,76 +156,74 @@ def scroll(images):
 def main():
   printScreen()
   images = load_images()
-  # scroll(images)
 
   while True:
     now = time.time()
-    if(clickBtn(images['connect-wallet'], timeout=1)):
-      print("Connected", now)
-      if(clickBtn(images['sign'], timeout=1)):
-        print("Sign", now)
+    connect_wallet = checkImage(images['connect-wallet'])
+    if connect_wallet:
+      clickBtn(images['connect-wallet'], 0, 0.9)
+      clickBtn(images['sign'], timeout=2)
+      print("connect-wallet", now)
 
+    play = checkImage(images['play'], threshold=0.9)
+    if play:
+      clickBtn(images['play'],0,0.9)  
+      print("play", now)
 
-    if(clickBtn(images['play'], timeout=1)):
-      print("Clicked play", now)
+    surrender = checkImage(images['surrender'], 0.9)
+    genesis = checkImage(images['genesis'], 0.9)
+    lose_confirm = checkImage(images['lose-confirm'], 0.9)
+    close = checkImage(images['close'], 0.9)
 
+    # CHECK IF YOU LOSE
+    if(lose_confirm):
+      clickBtn(images['lose-confirm'],0,0.9)
+      print("lose-confirm", now)
 
-    if(clickBtn(images['ok'], timeout=1)):
-      print("Clicked ok", now)
+    # CHECK IF NEED CLOSE
+    if(close):
+      clickBtn(images['close'],0,0.9)
+      print("close", now)
 
-
-    if(clickBtn(images['victory-confirm'], timeout=0.5, threshold=0.8)):
-      print("victory-confirm", now)
-
-      
-    if(clickBtn(images['close'], timeout=0.2, threshold=0.9)):
-      print("CLOSE")    
-
-    # if(clickBtn(images['no-ammo'], timeout=0.2)):
-    #   print(pyautogui.position())
-    #   if(clickBtn(images['remove-spaceship'], timeout=1)):
-    #     print("Remove Spaceship", now)
-
-
-    if(clickBtn(images['newest'], timeout=1)):
-      if(clickBtn(images['max-ammo'], timeout=1,threshold = 0.9)):
-        print("SET MAX AMMO", now)
+    if(surrender):
+      ## IN BATTLE
+      # CHECK IF THE BATTLE IS OVER
+      victory_confirm = checkImage(images['victory-confirm'], 0.8)
+      if(victory_confirm):
+        clickBtn(images['victory-confirm'],0,0.7)
+        print("victory-confirm", now)
     
-    if(clickBtn(images['team'], timeout=0.5, threshold = 1)):
-      print("strategy-team", now)
-      if(clickBtn(images['fight-boss'], timeout=0.8)):
-        print("team-send-to-boss", now)
-    else:    
-      if(clickBtn(images['send-to-fight'], timeout=0.5, threshold=0.9)):
-        print("send-to-fight", now)
-        # if(clickBtn(images['fight-boss'], timeout=0.8)):
-      else:
-        print("no send-to-fight", now)
-        if(clickBtn(images['fight-boss'], timeout=0.2)):
-          print("send-to-boss", now)
- 
-
-
-    if(clickBtn(images['surrender-boss'], timeout=0.8, threshold = 1)):
-      if(clickBtn(images['surrender'], timeout=0.8)):
-       if(clickBtn(images['surrender-confirm'], timeout=0.8)):
+      
+      # SURRENDER WHEN IT FINDS THE BOSS
+      surrender_boss = checkImage(images['surrender-boss'], 1)  
+      if(surrender_boss):
+        clickBtn(images['surrender'], 0, 0.8)
+        clickBtn(images['surrender-confirm'], 0.5, 0.8)
         print("surrender-confirm", now)
 
-
-    if(clickBtn(images['spaceship-empty'], timeout=1, threshold = 0.95)):
-      print("spaceship-empty", now)    
-      if(clickBtn(images['rocket'], timeout=0.8, threshold = 0.9)):
-        print("SEND TO REPAIR", now)
-
-
-    if(clickBtn(images['lose-confirm'], timeout=0.8, threshold = 0.9)):
-      print("lose-confirm", now)
-      if(clickBtn(images['rocket'], timeout=0.8, threshold = 0.9)):
-        print("SEND TO REPAIR", now)
-
-   
-
-
+      # CHECK FOR EMPTY SPACESHIPS
+      spaceship_empty = checkImage(images['spaceship-empty'], 0.95)
+      if(spaceship_empty):
+        clickBtn(images['rocket'], 0, 0.9)
+      
+  
+    if(genesis):
+      ## IN HOME
+      team = checkImage(images['team'], 1)
+      send_to_fight = checkImage(images['send-to-fight'], 0.9)
+      # CHECK TARGET TEAM
+      if(team):
+        clickBtn(images['fight-boss'],0,0.8)
+        print("fight-boss", now)
+      else: 
+        # SEND TO FIGHT
+        if(send_to_fight):
+          clickBtn(images['send-to-fight'],0,0.9)
+        else:  
+          # REMOVE SPACESHIPs
+          remove = checkImage(images['remove-spaceship'], 0.9)
+          if(remove): 
+            clickBtn(images['remove-spaceship'],0,0.9)
 
       
 if __name__ == '__main__':
